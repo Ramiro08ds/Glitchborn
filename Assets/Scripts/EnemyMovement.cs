5a79eeb57f9db04d329d3aa9ceae2c9304be9c28
Martinaaronwu <166827155+Martinaaronwu@users.noreply.github.com>
2025-08-25 09:45:57 -0300

    navigation in enemy y nueva cueva

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMovement : MonoBehaviour
+{   
+    public Transform destino;
+    private NavMeshAgent agente;
+
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+    }
+     void Update()
+    {
+            agente.SetDestination(destino.position);
+    }
+
+}
